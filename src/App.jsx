c23212d27745641ebbfca51b969564f9276b2b5c
Priyanshu Thakur <priyanshut964@gmail.com>
2024-02-15 03:22:38 +0530

    initial-production-ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from './pages/Home'
 import About from './pages/About'
